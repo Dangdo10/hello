ac34ddee6108c241497e8c22ef269160b4774567
Dang Van Do <do.dv@dsc.com.vn>
2025-07-18 00:20:14 +0700

    refactor 4

--- src/main/java/com/java/dangdo1198/project/controller/HelloController.java
+++ src/main/java/com/java/dangdo1198/project/controller/HelloController.java
@@ -1,13 +1,19 @@
 package com.java.dangdo1198.project.controller;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
+@Log4j2
 public class HelloController {
     @GetMapping("/api/sayHello")
     public String greeting() {
         System.out.println("dang van do");
+        log.info("Dang van do");
         return "hello";
     }
 }
