9af24c712716215fa8c005cd9e3dac89859cc990
Dang Van Do <do.dv@dsc.com.vn>
2025-07-18 00:14:02 +0700

    refactor 3

--- src/main/java/com/java/dangdo1198/project/controller/HelloController.java
+++ src/main/java/com/java/dangdo1198/project/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/api/sayHello")
     public String greeting() {
+        System.out.println("dang van do");
         return "hello";
     }
 }
