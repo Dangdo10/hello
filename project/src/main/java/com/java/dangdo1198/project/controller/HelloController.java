82c01b0e05ccea6cb94bcd42d412acae06075156
Dang Van Do <do.dv@dsc.com.vn>
2025-07-17 23:16:14 +0700

    push project

---
+++ project/src/main/java/com/java/dangdo1198/project/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.java.dangdo1198.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/sayHello")
+    public String greeting() {
+        return "hello";
+    }
+}
---
+++ project/src/test/java/com/java/dangdo1198/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.dangdo1198.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
